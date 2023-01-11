967e11d79bc42a3dae9dc7efec23fc3600198343
shahboz <107809563+sh4hboz@users.noreply.github.com>
2023-01-11 11:14:02 +0500

    last edit

--- main.js
+++ main.js
@@ -11,4 +11,21 @@ $(document).ready(function(){
     $('.payment form .login_input').on('input', function(e){
         $('.check_login').css('display', 'block')
     })
+
+    let indexThreeForm = $('.indexthree form');
+
+    indexThreeForm.on('input','input',function(e){
+
+        let form = indexThreeForm;
+        let login = indexThreeForm.find('.login_input');
+        let amount = form.find('.amount')
+        let agreement = form.find('.form-check-input');
+        
+        if(login.val().length >= 3 && amount.val() >= 100 && agreement.prop('checked')){
+            form.find('button').removeClass('disabled').attr('disabled',false);
+        }else{
+            form.find('button').addClass('disabled').attr('disabled',true);
+        }
+
+    })
 })
